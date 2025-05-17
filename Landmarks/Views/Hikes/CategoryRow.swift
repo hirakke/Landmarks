a021d2e3d742affcbc953bf3d214c94a8671a1c1
Keiju Hiramoto <hiramoto.keiju@gmail.com>
2025-05-17 19:37:35 +0900

    Complex Interface

---
+++ Landmarks/Views/Hikes/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Keiju Hiramoto on 2025/05/17.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
